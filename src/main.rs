92d9120ae1366932b50d620b363ab4011af47b6a
moppediert <moppediert@gmail.com>
2023-11-27 19:31:12 +0100

    read file
    Signed-off-by: moppediert <moppediert@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{path::PathBuf, env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let project_dir = env!("CARGO_MANIFEST_DIR");
+    let mut path: PathBuf = [project_dir, "input"].iter().collect();
+
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() < 4 {
+        panic!("cmd year day problem");
+    }
+    let year = &args[1];
+    let day = &args[2];
+    let problem = &args[3];
+    path.push(year);
+    path.push(day);
+    path.push(problem);
+
+    println!("path {}", read_input(path));
+}
+
+fn read_input(path: PathBuf) -> String {
+    let input: String = fs::read_to_string(path).unwrap();
+    input
 }
