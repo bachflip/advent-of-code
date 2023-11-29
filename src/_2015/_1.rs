2c1684223f8aeb677293f12998c829ffa6dde524
moppediert <moppediert@gmail.com>
2023-11-29 21:13:39 +0100

    this macro is not doing it
    Signed-off-by: moppediert <moppediert@gmail.com>

---
+++ src/_2015/_1.rs
@@ -0,0 +1,3 @@
+pub fn solve() {
+    println!("hello from day 1 of year 2015");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+pub mod _2015;
+
 use std::{path::PathBuf, env, fs};
+
 
 fn main() {
     let project_dir = env!("CARGO_MANIFEST_DIR");
@@ -8,17 +11,30 @@ fn main() {
     if args.len() < 4 {
         panic!("cmd year day problem");
     }
+
     let year = &args[1];
     let day = &args[2];
     let problem = &args[3];
     path.push(year);
     path.push(day);
     path.push(problem);
+    let input = read_input(path);
+    _2015::_1::solve();
 
-    println!("path {}", read_input(path));
+    solve!(year, day, problem);
+    
 }
 
 fn read_input(path: PathBuf) -> String {
     let input: String = fs::read_to_string(path).unwrap();
     input
+}
+
+#[macro_export]
+macro_rules! solve {
+    ( $year:path, $day:path, $problem:ident ) => {
+        {
+            $year::$day::solve();
+        }
+    };
 }
